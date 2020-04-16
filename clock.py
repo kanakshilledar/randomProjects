c970db2f102484e24784ec54728e90839a933be5
kanakshilledar <56501211+kanakshilledar@users.noreply.github.com>
2020-04-16 22:08:03 +0530

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,18 +1,36 @@
-# This is a clock hardcoded in python
+# This is a curTime hardcoded in python
 # Just using a few libraries
 # Program Written By Kanak Shilledar
 
 # importing libraries
+import datetime
 import time
 from os import system, name
 
-# initializing the clock
-print('Clock Started: ')
-clock = [00, 00, 00]
+# converting the datetime datatype to list
+curTime = datetime.datetime.now()
+curTime = str(curTime)
+curTime = curTime[11:19]
+curTime = list(curTime)
+for ele in range(len(curTime) - 2):
+    if curTime[ele] == ':':
+        curTime.pop(ele)
+    
+for ele in range(0, len(curTime) - 1):
+    curTime[ele] = curTime[ele] + curTime[ele + 1]
+for i in range(1, 4):
+    curTime.pop(i)
+for i in range(len(curTime)):
+    curTime[i] = int(curTime[i])
 
-# clock[0] = hours
-# clock[1] = minutes
-# clock[2] = seconds
+# adding one sec to reduce the time lost
+curTime[2] += 1
+# initializing the curTime
+print('Clock Started: ')
+
+# curTime[0] = hours
+# curTime[1] = minutes
+# curTime[2] = seconds
 
 # main algorithm
 while True:
@@ -20,15 +38,15 @@ while True:
     # clearing the screen
     if name == 'nt':
         _ = system('cls')
-    clock[2] += 1
+    curTime[2] += 1
     # for minute
-    if clock[2] == 60:
-        clock[2] = 0
-        clock[1] += 1
+    if curTime[2] == 60:
+        curTime[2] = 0
+        curTime[1] += 1
     # for hour
-    if clock[1] == 60:
-        clock[1] = 0
-        clock[0] +=1
+    if curTime[1] == 60:
+        curTime[1] = 0
+        curTime[0] +=1
     
-    # printing the clock
-    print(clock)
+    # printing the curTime
+    print(curTime)
